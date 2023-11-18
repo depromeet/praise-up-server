392ecf5a27dede19e6a992b106979e2e8b323f70
yunyoung <yunyoung1819@leznin.com>
2023-11-18 15:37:18 +0900

    feat: Project init setting

---
+++ src/test/java/com/praise/praisejail/PraiseJailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praise.praisejail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraiseJailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
