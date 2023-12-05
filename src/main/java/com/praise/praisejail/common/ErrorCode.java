491c551a80bcd957b1581dab0576017809bc4ab1
YUN YOUNG <yunyoung1819@hanmail.net>
2023-12-06 00:57:15 +0900

    :sparkles: feature: Add ErrorCode (#21)

---
+++ src/main/java/com/praise/praisejail/common/ErrorCode.java
@@ -0,0 +1,39 @@
+package com.praise.praisejail.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    /*
+     * 400 BAD_REQUEST: 잘못된 요청
+     */
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "C-000", "Bad Request"),
+
+    /*
+     * 404 NOT_FOUND: 리소스를 찾을 수 없음
+     */
+    NOT_FOUND(HttpStatus.NOT_FOUND, "C-001", "Not Found the Contents"),
+
+    /*
+     * 405 METHOD_NOT_ALLOWED: 허용되지 않은 Request Method 호출
+     */
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "C-002", "Method Not Allowed"),
+
+    /*
+     * 500 INTERNAL_SERVER_ERROR: 내부 서버 오류
+     */
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "C-003", "Internal Server Error"),
+
+    /*
+     * 400 BAD_REQUEST: 요청 인자가 유효하지 않음
+     */
+    METHOD_ARGUMENT_NOT_VALID(HttpStatus.BAD_REQUEST, "C-004", "Method Argument Not Valid");
+
+    private final HttpStatus httpStatus;
+    private final String errorCode;
+    private final String errorMessage;
+}
