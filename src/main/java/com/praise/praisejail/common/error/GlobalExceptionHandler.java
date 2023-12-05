ae4f17e0f8f214e9893ce73e4e6edfb8bbd31953
Hoonsub Kim <kimhunsope@naver.com>
2023-12-06 00:56:45 +0900

    :sparkles: feature: Create GlobalException Handler (#20)

---
+++ src/main/java/com/praise/praisejail/common/error/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.praise.praisejail.common.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(final RuntimeException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("RuntimeException Handler: " + exception.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(final Exception exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Exception Handler: " + exception.getMessage());
+    }
+}
