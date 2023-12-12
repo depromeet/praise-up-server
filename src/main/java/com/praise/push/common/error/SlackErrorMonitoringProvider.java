1b1c3757a66046c8c799c2e5f0001b83c297df57
Khsrla9806 <kimhunsope@naver.com>
2023-12-13 00:23:49 +0900

    :recycle: refactor: change method name in SlackErrorMonitoringProvider

--- src/main/java/com/praise/push/common/error/SlackErrorMonitoringProvider.java
+++ src/main/java/com/praise/push/common/error/SlackErrorMonitoringProvider.java
@@ -18,30 +18,30 @@ public class SlackErrorMonitoringProvider implements ErrorMonitoringProvider {
     @Override
     public void push(final Exception exception, final HttpServletRequest request) {
         SlackApi slackApi = new SlackApi(webHookUrl);
-        slackApi.call(getSlackMessage(exception, request));
+        slackApi.call(createSlackMessage(exception, request));
     }
 
     /**
      * @return Slack error message with exception and request
      */
-    private SlackMessage getSlackMessage(final Exception exception, final HttpServletRequest request) {
+    private SlackMessage createSlackMessage(final Exception exception, final HttpServletRequest request) {
         return new SlackMessage()
                 .setUsername("Praise-Push Error Monitoring Bot")
                 .setText("5xx Error is Detected")
-                .setAttachments(getSlackAttachments(exception, request))
+                .setAttachments(createSlackAttachments(exception, request))
                 .setIcon(":rotating_light:");
     }
 
     /**
      * @return Slack attachments required to create the Slack message
      */
-    private List<SlackAttachment> getSlackAttachments(final Exception exception, final HttpServletRequest request) {
+    private List<SlackAttachment> createSlackAttachments(final Exception exception, final HttpServletRequest request) {
         SlackAttachment slackAttachment = new SlackAttachment()
                 .setFallback("Error")
                 .setColor("danger")
                 .setTitleLink(request.getContextPath())
                 .setText(Arrays.toString(exception.getStackTrace()))
-                .setFields(getSlackFields(request));
+                .setFields(createSlackFields(request));
 
         return List.of(slackAttachment);
     }
@@ -49,7 +49,7 @@ public class SlackErrorMonitoringProvider implements ErrorMonitoringProvider {
     /**
      * @return Slack Fields to Create Slack Attachment Details
      */
-    private List<SlackField> getSlackFields(final HttpServletRequest request) {
+    private List<SlackField> createSlackFields(final HttpServletRequest request) {
         return List.of(
                 createSlackField("Request URL", request.getRequestURI()),
                 createSlackField("Request Method", request.getMethod()),
