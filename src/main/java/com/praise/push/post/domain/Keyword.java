20c6e4be1e259574d419157d240704fe4112a272
yunyoung <yunyoung1819@leznin.com>
2023-12-13 21:44:48 +0900

    :sparkles: Add Comment Entity

--- src/main/java/com/praise/push/post/domain/Keyword.java
+++ src/main/java/com/praise/push/post/domain/Keyword.java
@@ -1,8 +1,10 @@
 package com.praise.push.post.domain;
 
+import com.praise.push.comment.domain.Comment;
 import com.praise.push.domain.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -37,5 +39,16 @@ public class Keyword extends BaseTimeEntity {
      */
     @OneToMany(mappedBy = "keyword")
     private List<Post> posts;
+
+    /**
+     * 댓글
+     */
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "post")
+    private List<Comment> comments;
+
+    /**
+     * 공개 여부
+     */
+    private Boolean visible;
 
 }
