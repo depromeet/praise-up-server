b718822a5d6605b973f990a619f1a18fcf85d97a
Hoonsub Kim <kimhunsope@naver.com>
2023-12-19 14:34:32 +0900

    ✨ feature: add slack error monitoring (#28)

--- src/main/java/com/praise/push/common/error/GlobalExceptionHandler.java
+++ src/main/java/com/praise/push/common/error/GlobalExceptionHandler.java
@@ -1,12 +1,18 @@
 package com.praise.push.common.error;
 
+import com.praise.push.common.monitoring.MonitoringProvider;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RequiredArgsConstructor
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private final MonitoringProvider monitoringProvider;
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(final RuntimeException exception) {
@@ -16,7 +22,9 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(final Exception exception) {
+    public ResponseEntity<String> handleException(final Exception exception, final HttpServletRequest request) {
+        monitoringProvider.push(exception, request);
+
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("Exception Handler: " + exception.getMessage());
