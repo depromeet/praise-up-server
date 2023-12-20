e48ccb4c2835023187cbe6cb169109e79fcea5f1
mywnajsldkf <mywnajsldkf@gmail.com>
2023-12-20 14:02:51 +0900

    :sparkles: Add createPost Dto (#36)

--- src/main/java/com/praise/push/post/application/port/in/CreatePostCommand.java
+++ src/main/java/com/praise/push/post/application/port/in/CreatePostCommand.java
@@ -1,25 +1,15 @@
 package com.praise.push.post.application.port.in;
 
-import com.praise.push.post.domain.Keyword;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Value;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Value
+@Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CreatePostCommand {
-    @NotNull
-    private final Long postId;
-
-    @NotNull
-    private final String title;
-
-    @NotNull
-    private final String content;
-
-    @NotNull
-    private final String imageUrl;
-
-    @NotNull
-    private final Keyword keyword;
+    private String title;
+    private String content;
+    private String imageUrl;
+    private String keyword;
 }
