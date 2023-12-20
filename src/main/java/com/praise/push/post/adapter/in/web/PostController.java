de72993d765b2b52b1c916233afb6013f04d97e6
mywnajsldkf <mywnajsldkf@gmail.com>
2023-12-20 22:34:16 +0900

    :sparkles: feat: add updatePost controller (#36)

--- src/main/java/com/praise/push/post/adapter/in/web/PostController.java
+++ src/main/java/com/praise/push/post/adapter/in/web/PostController.java
@@ -2,6 +2,7 @@ package com.praise.push.post.adapter.in.web;
 
 import com.praise.push.post.application.port.in.CreatePostCommand;
 import com.praise.push.post.application.port.in.PostUseCase;
+import com.praise.push.post.application.port.in.UpdatePostCommand;
 import com.praise.push.post.application.port.out.PostResponse;
 import com.praise.push.post.domain.Post;
 import lombok.RequiredArgsConstructor;
@@ -33,5 +34,19 @@ public class PostController {
     @DeleteMapping
     void deletePost(@RequestParam(name = "postId") Long postId) {
         postUseCase.deletePost(postId);
+    }
+
+    @PatchMapping
+    PostResponse updatePost(@RequestParam(name = "postId") Long postId,
+                            @RequestBody UpdatePostCommand command) {
+        postUseCase.updatePost(postId, command);
+        Post post = postUseCase.findPost(postId);
+        return PostResponse.builder()
+                .title(post.getTitle())
+                .content(post.getContent())
+                .imageUrl(post.getImageUrl())
+                .keyword(post.getKeyword().getKeyword())
+                .visible(post.getVisible())
+                .build();
     }
 }
