3e9bceb4df507424f519b1c8632a8b4d05743c0d
mywnajsldkf <mywnajsldkf@gmail.com>
2023-12-20 21:32:27 +0900

    :sparkles: add findPost api (#36)

--- src/main/java/com/praise/push/post/adapter/in/web/PostController.java
+++ src/main/java/com/praise/push/post/adapter/in/web/PostController.java
@@ -2,11 +2,10 @@ package com.praise.push.post.adapter.in.web;
 
 import com.praise.push.post.application.port.in.CreatePostCommand;
 import com.praise.push.post.application.port.in.PostUseCase;
+import com.praise.push.post.application.port.out.PostResponse;
+import com.praise.push.post.domain.Post;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/praise-push/api/v1")
@@ -17,5 +16,17 @@ public class PostController {
     @PostMapping
     void createPost(@RequestBody CreatePostCommand command) {
         postUseCase.createPost(command);
+    }
+
+    @GetMapping
+    PostResponse findPost(@RequestParam(name = "postId") Long postId) {
+        Post post = postUseCase.findPost(postId);
+        return PostResponse.builder()
+                .title(post.getTitle())
+                .content(post.getContent())
+                .imageUrl(post.getImageUrl())
+                .keyword(post.getKeyword().getKeyword())
+                .visible(post.getVisible())
+                .build();
     }
 }
