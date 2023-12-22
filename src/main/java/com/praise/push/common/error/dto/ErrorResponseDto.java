2b10319cedbb42282ea70d51854236eb7bff0803
Khsrla9806 <kimhunsope@naver.com>
2023-12-23 00:21:54 +0900

    :sparkles: feature: add error response

---
+++ src/main/java/com/praise/push/common/error/dto/ErrorResponseDto.java
@@ -0,0 +1,48 @@
+package com.praise.push.common.error.dto;
+
+import com.praise.push.common.ErrorCode;
+import lombok.Builder;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record ErrorResponseDto(
+        LocalDateTime timestamp,
+        Integer status,
+        String code,
+        String message
+) {
+
+    /**
+     * returns error response with ErrorCode
+     */
+    public static ResponseEntity<ErrorResponseDto> build(ErrorCode errorCode) {
+        ErrorResponseDto errorResponseDto = ErrorResponseDto.builder()
+                .timestamp(LocalDateTime.now())
+                .status(errorCode.getHttpStatus().value())
+                .code(errorCode.getErrorCode())
+                .message(errorCode.getErrorMessage())
+                .build();
+
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(errorResponseDto);
+    }
+
+    /**
+     * returns error response with ErrorCode and custom error message
+     */
+    public static ResponseEntity<ErrorResponseDto> build(ErrorCode errorCode, String message) {
+        ErrorResponseDto errorResponseDto = ErrorResponseDto.builder()
+                .timestamp(LocalDateTime.now())
+                .status(errorCode.getHttpStatus().value())
+                .code(errorCode.getErrorCode())
+                .message(message)
+                .build();
+
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(errorResponseDto);
+    }
+}
