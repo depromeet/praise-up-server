f6652ad27d1a75eda270cc4868d1b1b4b3c86a15
Khsrla9806 <kimhunsope@naver.com>
2023-12-23 00:23:03 +0900

    :recycle: refactor: apply ErrorResponseDto to GlobalExceptionHandler

--- src/main/java/com/praise/push/common/error/GlobalExceptionHandler.java
+++ src/main/java/com/praise/push/common/error/GlobalExceptionHandler.java
@@ -1,32 +1,32 @@
 package com.praise.push.common.error;
 
+import com.praise.push.common.ErrorCode;
+import com.praise.push.common.error.dto.ErrorResponseDto;
 import com.praise.push.common.monitoring.MonitoringProvider;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLException;
+
+@Slf4j
 @RequiredArgsConstructor
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private final MonitoringProvider monitoringProvider;
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleRuntimeException(final RuntimeException exception) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body("RuntimeException Handler: " + exception.getMessage());
-    }
-
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(final Exception exception, final HttpServletRequest request) {
+    public ResponseEntity<ErrorResponseDto> handleException(
+            final Exception exception,
+            final HttpServletRequest request
+    ) {
+        log.error("Exception: {}, Request URI: {}", exception.getMessage(), request.getRequestURL());
         monitoringProvider.push(exception, request);
 
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Exception Handler: " + exception.getMessage());
+        return ErrorResponseDto.build(ErrorCode.INTERNAL_SERVER_ERROR);
     }
 }
