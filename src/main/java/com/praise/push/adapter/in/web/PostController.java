e88cc1213d78d86ba491fd763f765056979faed7
mywnajsldkf <mywnajsldkf@gmail.com>
2024-01-14 03:29:52 +0900

    :sparkles: feature: post pagination(#64)

--- src/main/java/com/praise/push/adapter/in/web/PostController.java
+++ src/main/java/com/praise/push/adapter/in/web/PostController.java
@@ -3,13 +3,17 @@ package com.praise.push.adapter.in.web;
 import com.praise.push.application.port.in.CreatePostCommand;
 import com.praise.push.application.port.in.PostUseCase;
 import com.praise.push.application.port.in.UpdatePostCommand;
+import com.praise.push.application.port.in.dto.PostSummaryResponseDto;
 import com.praise.push.application.port.out.PostResponse;
+import com.praise.push.common.model.ResponseDto;
 import com.praise.push.domain.Post;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,6 +28,17 @@ class PostController {
     @PostMapping(value = "/posts", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     void createPost(@ModelAttribute CreatePostCommand command) {
         postUseCase.createPost(command);
+    }
+
+    @Operation(summary = "게시글 목록 조회")
+    @ApiResponse(responseCode = "200", description = "게시글 목록 조회 성공")
+    @GetMapping("/posts")
+    ResponseEntity<Page<PostSummaryResponseDto>> getPosts(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "24") Integer size
+    ) {
+        Page<PostSummaryResponseDto> posts = postUseCase.getPosts(page, size);
+        return ResponseDto.ok(posts);
     }
 
     @Operation(summary = "게시글 단건 조회")
