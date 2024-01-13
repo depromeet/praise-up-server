3c8e0efea6d7f0067d8e167e453a1014ee30c968
yunyoung <yunyoung1819@leznin.com>
2024-01-14 00:57:03 +0900

    :sparkles: add cors annotation

--- src/main/java/com/praise/push/adapter/in/web/LoginController.java
+++ src/main/java/com/praise/push/adapter/in/web/LoginController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 @Tag(name = "Login", description = "Login/Logout API")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LoginController  {
     private final LoginService loginService;
     private final UserService userService;
--- src/main/java/com/praise/push/adapter/in/web/UserController.java
+++ src/main/java/com/praise/push/adapter/in/web/UserController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 @Tag(name = "Users", description = "User API")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
     private final UserService userService;
 
