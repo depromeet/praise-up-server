fd749858dba099be9c704cb8ca9cbd0b7765d36c
yunyoung <yunyoung1819@leznin.com>
2024-01-13 23:35:26 +0900

    :sparkles: add user delete with withdrawal reason

--- src/main/java/com/praise/push/application/service/UserService.java
+++ src/main/java/com/praise/push/application/service/UserService.java
@@ -1,11 +1,13 @@
 package com.praise.push.application.service;
 
 import com.praise.push.adapter.out.persistence.UserRepository;
+import com.praise.push.adapter.out.persistence.WithdrawalReasonRepository;
 import com.praise.push.application.port.out.KakaoAccount;
 import com.praise.push.application.port.out.LoginResponse;
 import com.praise.push.application.port.out.Profile;
 import com.praise.push.application.port.out.UserResponse;
 import com.praise.push.domain.User;
+import com.praise.push.domain.WithdrawalReason;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +20,7 @@ import org.webjars.NotFoundException;
 @Slf4j
 public class UserService {
     private final UserRepository userRepository;
+    private final WithdrawalReasonRepository withdrawalReasonRepository;
 
     public LoginResponse doSocialLogin(KakaoAccount kakaoAccount) {
         Profile profile = kakaoAccount.getProfile();
@@ -28,6 +31,7 @@ public class UserService {
             return new LoginResponse(user.get());
         } else {
             User newUser = User.builder()
+                .email(email)
                 .nickname(profile.getNickname())
                 .profileImage(profile.getProfile_image_url())
                 .build();
@@ -36,18 +40,33 @@ public class UserService {
         }
     }
 
+    @Transactional(readOnly = true)
     public UserResponse getUserById(Long id) {
         User user = userRepository.findById(id)
-            .orElseThrow(() -> new RuntimeException("User is Not Found."));
+            .orElseThrow(() -> new RuntimeException("User is not Found."));
         return new UserResponse(user);
     }
 
     @Transactional
     public UserResponse changeNickname(Long id, String nickname) {
         User user = userRepository.findById(id)
-            .orElseThrow(() -> new NotFoundException("User is Not found"));
+            .orElseThrow(() -> new NotFoundException("User is not found"));
 
         User changeUser = user.changeNickname(nickname);
         return new UserResponse(changeUser);
+    }
+
+    @Transactional
+    public void deleteUser(Long id, String reason) {
+        User user = userRepository.findById(id)
+            .orElseThrow(() -> new NotFoundException("User is not found"));
+
+        WithdrawalReason withdrawalReason = WithdrawalReason.builder()
+            .userId(user.getId())
+            .reason(reason)
+            .build();
+
+        withdrawalReasonRepository.save(withdrawalReason);
+        userRepository.deleteById(id);
     }
 }
