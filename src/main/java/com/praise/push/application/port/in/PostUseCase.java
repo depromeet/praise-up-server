83d56dd722177ca5ef57e7464bbde9dfc7aea43a
mywnajsldkf <mywnajsldkf@gmail.com>
2024-01-14 03:30:16 +0900

    :sparkles: feature: post pagination service(#64)

--- src/main/java/com/praise/push/application/port/in/PostUseCase.java
+++ src/main/java/com/praise/push/application/port/in/PostUseCase.java
@@ -1,9 +1,13 @@
 package com.praise.push.application.port.in;
 
+import com.praise.push.application.port.in.dto.PostSummaryResponseDto;
 import com.praise.push.domain.Post;
+import org.springframework.data.domain.Page;
 
 public interface PostUseCase {
     boolean createPost(CreatePostCommand command);
+
+    Page<PostSummaryResponseDto> getPosts(Integer page, Integer size);
 
     Post findPost(Long postId);
 
