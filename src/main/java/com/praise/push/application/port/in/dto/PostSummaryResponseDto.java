8a66aafec1a07690ab2c8084753ef0ad88d1eb86
mywnajsldkf <mywnajsldkf@gmail.com>
2024-01-18 07:54:52 +0900

    :recycle: refactor: postCreatedDate type String to date format(#105)

--- src/main/java/com/praise/push/application/port/in/dto/PostSummaryResponseDto.java
+++ src/main/java/com/praise/push/application/port/in/dto/PostSummaryResponseDto.java
@@ -1,20 +1,22 @@
 package com.praise.push.application.port.in.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.praise.push.domain.model.PostWithCommentCount;
 import lombok.Builder;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 @Builder
 public record PostSummaryResponseDto(
-        String date,
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        Date postCreatedDate,
         String imageUrl,
         String keyword,
         Long commentCount
 ) {
     public static PostSummaryResponseDto fromVisibleEntity(PostWithCommentCount postWithCommentCount) {
         return PostSummaryResponseDto.builder()
-                .date(convertCreatedDate(postWithCommentCount.getPost().getCreatedDate()))
+                .postCreatedDate(java.sql.Timestamp.valueOf(postWithCommentCount.getPost().getCreatedDate()))
                 .imageUrl(postWithCommentCount.getPost().getImageUrl())
                 .keyword(postWithCommentCount.getPost().getKeyword().getKeyword())
                 .commentCount(postWithCommentCount.getCommentCount())
@@ -23,39 +25,10 @@ public record PostSummaryResponseDto(
 
     public static PostSummaryResponseDto fromInvisibleEntity(PostWithCommentCount postWithCommentCount) {
         return PostSummaryResponseDto.builder()
-                .date(String.valueOf(postWithCommentCount.getPost().getCreatedDate()))
+                .postCreatedDate(java.sql.Timestamp.valueOf(postWithCommentCount.getPost().getCreatedDate()))
                 .imageUrl(postWithCommentCount.getPost().getImageUrl())
                 .keyword(postWithCommentCount.getPost().getKeyword().getKeyword())
                 .commentCount(postWithCommentCount.getCommentCount())
                 .build();
-    }
-
-    private static String convertCreatedDate(LocalDateTime createdDate) {
-        StringBuilder convertedDate = new StringBuilder();
-
-        int year = createdDate.getYear();
-        int month = createdDate.getMonthValue();
-        int day = createdDate.getDayOfMonth();
-
-
-        convertedDate.append(year % 100);
-        convertedDate.append(".");
-
-        if (month < 10) {
-            convertedDate.append("0");
-            convertedDate.append(month);
-        } else {
-            convertedDate.append(month);
-        }
-
-        convertedDate.append(".");
-
-        if (day < 10) {
-            convertedDate.append("0");
-            convertedDate.append(day);
-        }
-        convertedDate.append(day);
-
-        return convertedDate.toString();
     }
 }
