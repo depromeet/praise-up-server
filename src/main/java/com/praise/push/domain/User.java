0a0bdf228ddedcc0e1e5228a7c9f15a120e19d26
yunyoung <yunyoung1819@leznin.com>
2024-01-17 22:27:40 +0900

    :sparkles: feature: create user table index

--- src/main/java/com/praise/push/domain/User.java
+++ src/main/java/com/praise/push/domain/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
@@ -17,7 +18,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "users")
+@Table(name = "users", indexes = {@Index(name = "idx_email", columnList = "email")})
 public class User extends BaseTimeEntity {
 
     /**
