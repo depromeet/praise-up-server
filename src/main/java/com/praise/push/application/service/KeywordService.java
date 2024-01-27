93014e5de6eba1a06e3091f659cf4e9d379ef0bd
Khsrla9806 <kimhunsope@naver.com>
2024-01-27 19:12:27 +0900

    :sparkles: feature: add update selected count of Keyword

--- src/main/java/com/praise/push/application/service/KeywordService.java
+++ src/main/java/com/praise/push/application/service/KeywordService.java
@@ -3,6 +3,7 @@ package com.praise.push.application.service;
 import com.praise.push.application.port.in.KeywordUseCase;
 import com.praise.push.application.port.in.dto.KeywordResponseDto;
 import com.praise.push.application.port.out.LoadKeywordPort;
+import com.praise.push.application.port.out.RecordKeywordPort;
 import com.praise.push.domain.Keyword;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ import java.util.List;
 @Service
 public class KeywordService implements KeywordUseCase {
     private final LoadKeywordPort loadKeywordPort;
+    private final RecordKeywordPort recordKeywordPort;
 
     @Override
     @Transactional(readOnly = true)
@@ -27,5 +29,17 @@ public class KeywordService implements KeywordUseCase {
         List<Keyword> recommendationKeywords = keywords.subList(0, size);
 
         return recommendationKeywords.stream().map(KeywordResponseDto::fromEntity).toList();
+    }
+
+    @Transactional
+    public void updateSelectedCount(Integer size) {
+        List<Keyword> keywords = loadKeywordPort.loadKeywords();
+        keywords.sort((k1, k2) -> k1.getSelectedCount() - k2.getSelectedCount());
+        if (size < keywords.size()) {
+            keywords = keywords.subList(0, size);
+        }
+
+        List<Long> keywordIds = keywords.stream().map(Keyword::getId).toList();
+        recordKeywordPort.updateSelectedCount(keywordIds);
     }
 }
