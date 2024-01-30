ceee870b5f20cfb88a6ed4400a8e606ab4f1e2f5
Khsrla9806 <kimhunsope@naver.com>
2024-01-30 13:05:51 +0900

    :sparkles: feature: add post update visible bulk query

--- src/main/java/com/praise/push/adapter/out/persistence/PostRepository.java
+++ src/main/java/com/praise/push/adapter/out/persistence/PostRepository.java
@@ -5,9 +5,11 @@ import com.praise.push.domain.model.PostWithCommentCount;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 interface PostRepository extends JpaRepository<Post, Long> {
@@ -30,4 +32,8 @@ interface PostRepository extends JpaRepository<Post, Long> {
             "GROUP BY p " +
             "ORDER BY p.id DESC")
     List<PostWithCommentCount> findUnreadPostsWithCommentCount(@Param("userId") Long userId);
+
+    @Modifying
+    @Query("UPDATE posts p SET p.visible = true WHERE p.visible = false and p.createdDate < :dateTime")
+    void updatePostsVisibleIsBeforeDateTime(@Param("dateTime") LocalDateTime dateTime);
 }
