ad646fc828bc7dd484866c3c6d60268bf186532f
JeonginKim <47661695+mywnajsldkf@users.noreply.github.com>
2024-02-02 18:31:21 +0900

    ♻️ Refactor: add return argument (#164)
    * 🐛 Fix: post check logic (#128)
    
    * :ambulance: hotfix: delete title argument(#64)
    
    * :wrench: fix: s3 upload return url bug(#64)
    
    * :sparkles: feature: post pagination(#64)
    
    * :sparkles: feature: post pagination service(#64)
    
    * :sparkles: feature: post load adapter(#64)
    
    - jpql을 활용하여 post와 연관관계에 있는 comment를 조회함
    
    * :recycle: refactor: @Entity annotation designate name value(#64)
    
    * :sparkles: feat: PostSummaryResponseDto(#64)
    
    * :sparkles: feat: apply changed list(#64)
    
    * :sparkles: feat: apply changed list(#64)
    
    * :recycle: refactor: add yml property(#64)
    
    * :recycle: refactor: apply ResponseEntity(#64)
    
    * :sparkles: feat: add isRead variable(#64)
    
    * :recycle: refactor: apply ResponseEntity(#64)
    
    * :sparkles: feature: update post isRead state(#64)
    
    * :recycle: refactor: depart invisible posts from posts(#95)
    
    * :bug: fix: add imageUrl field(#95)
    
    * :sparkles: feat: return invisiblePosts(#95)
    
    * :bug: fix: add imageUrl(#95)
    
    - invisibleEntity를 만들 때, imageUrl을 가져오는 코드 추가
    
    * :sparkles: feat: add get invisible post method(#95)
    
    - 확인할 수 없는 게시글을 확인하는 기능 추가
    
    * :recycle: refactor: filter visible posts(#95)
    
    * :sparkles: feat: filter invisible query(#95)
    
    * :wrench: chore: todo memo(#95)
    
    * :recycle: refactor: edit responseCode to 201(#98)
    
    * :sparkles: feature: post API - connect post with userId(#98)
    
    * :sparkles: feature: add userId query parameter(#98)
    
    * :sparkles: feature: add userId query parameter - service(#98)
    
    * :sparkles: feature: add userId query parameter - persistence layer(#98)
    
    * :sparkles: feature: add select query connecting 3tables (#98)
    
    * :bug: fix: return post createdDate(#105)
    
    * :recycle: refactor: postCreatedDate type String to date format(#105)
    
    * :recycle: refactor: add postId(#112)
    
    * :recycle: refactor: add userNickname(#115)
    
    * :bug: fix: change getPosts argument(visible -> isRead) (#118)
    
    * :wrench: chores: remove (required = false) option
    
    * :bug: Fix: change select query column(visible -> isRead) (#118)
    
    * :bug: Fix: add visible check query
    
    * :recycle: Refactor: rename postResponseDto, add postId parameter
    
    * :recycle: Refactor: allow nullable body argument
    
    * :recycle: Refactor: return Post type
    
    * :recycle: Refactor: return PostResponseDto type
    
    * :bug: Fix: add check post.visible value(#127)
    
    * :recycle: Refactor: add PostThumbnailResponseDto field(postCreatedTime, visible) (#160)
    
    ---------
    
    Co-authored-by: Hoonsub Kim <kimhunsope@naver.com>

--- src/main/java/com/praise/push/application/port/in/dto/PostThumbnailResponseDto.java
+++ src/main/java/com/praise/push/application/port/in/dto/PostThumbnailResponseDto.java
@@ -11,27 +11,33 @@ import java.util.Date;
 public record PostThumbnailResponseDto(
         @JsonFormat(pattern = "yyyy-MM-dd")
         Date postCreatedDate,
+        Date postCreatedTime,
         Long postId,
         String imageUrl,
         String keyword,
+        Boolean visible,
         Long commentCount
 ) {
     public static PostThumbnailResponseDto fromReadEntity(PostWithCommentCount postWithCommentCount) {
         return PostThumbnailResponseDto.builder()
                 .postCreatedDate(Timestamp.valueOf(postWithCommentCount.getPost().getCreatedDate()))
+                .postCreatedTime(Timestamp.valueOf(postWithCommentCount.getPost().getCreatedDate()))
                 .postId(postWithCommentCount.getPost().getId())
                 .imageUrl(postWithCommentCount.getPost().getImageUrl())
                 .keyword(postWithCommentCount.getPost().getKeyword().getKeyword())
+                .visible(postWithCommentCount.getPost().getVisible())
                 .commentCount(postWithCommentCount.getCommentCount())
                 .build();
     }
 
     public static PostThumbnailResponseDto fromUnReadEntity(PostWithCommentCount postWithCommentCount) {
         return PostThumbnailResponseDto.builder()
-                .postCreatedDate(java.sql.Timestamp.valueOf(postWithCommentCount.getPost().getCreatedDate()))
+                .postCreatedDate(Timestamp.valueOf(postWithCommentCount.getPost().getCreatedDate()))
+                .postCreatedTime(Timestamp.valueOf(postWithCommentCount.getPost().getCreatedDate()))
                 .postId(postWithCommentCount.getPost().getId())
                 .imageUrl(postWithCommentCount.getPost().getImageUrl())
                 .keyword(postWithCommentCount.getPost().getKeyword().getKeyword())
+                .visible(postWithCommentCount.getPost().getVisible())
                 .commentCount(postWithCommentCount.getCommentCount())
                 .build();
     }
